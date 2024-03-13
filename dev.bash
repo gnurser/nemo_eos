0403b8c60e4d42e11c8c21e3b15d4a231b5406cb
George Nurser <agn@noc.ac.uk>
2024-03-13 15:48:11 +0000

    dev.bash now includes optional argument isolate_build

--- dev.bash
+++ dev.bash
@@ -1,9 +1,48 @@
-cd ~/Dropbox/VC_code/nemo_eos
-mamba activate big
-site_packages=$(get_site_packages) # => /Users/agn/miniforge3/envs/big/python3.10/site-packages
-mamba env create -f  nemo_eos_dev.yml
-mamba activate nemo_eos_dev
-meson setup builddir -Dprefix=/Users/agn/miniforge3/envs/big
-meson compile -C builddir
-meson install -C builddir
-# Note *do not use* rest of $site_packages i.e python3.10/site-packages bit
+echo $1
+function isolate_build(){
+    echo doing isolated build
+    #make proper build environment with mamba
+    git clone theia:~agn/Bare/nemo_eos.git
+    cd nemo_eos
+    mamba install compilers numpy
+    export PREFIX_OUT=$CONDA_PREFIX
+    mamba env create -f  nemo_eos_dev.yml
+    mamba activate nemo_eos_dev
+    meson setup builddir --prefix $PREFIX_OUT # /Users/agn/miniforge3/envs/$mamba_env
+    meson compile -C builddir
+    meson install -C builddir
+    mamba remove --name nemo_eos_dev --all
+}
+
+# install_rpath needs to be specified in the definition of py.extension_module in meson.build
+#   ... the line below does not work.
+# meson install -C builddir -Dinstall_rpath=$CONDA_PREFIX/lib
+#note the discussion on "fixing' rpaths at
+# https://github.com/mesonbuild/meson-python/pull/340
+# https://github.com/mesonbuild/meson/issues/6541
+#
+
+function run_build(){
+    #just add build requirements to mamba run environment
+    echo doing run build
+    git clone theia:~agn/Bare/nemo_eos.git
+    cd nemo_eos
+    mamba install compilers numpy
+    mamba install meson-python pkg-config #=>
+
+# + pyproject-metadata    0.7.1  pyhd8ed1ab_0  conda-forge     Cached
+# + colorama              0.4.6  pyhd8ed1ab_0  conda-forge     Cached
+# + ninja                1.11.1  hb8565cd_0    conda-forge     Cached
+# + meson                 1.4.0  pyhd8ed1ab_0  conda-forge      634kB
+# + meson-python         0.15.0  pyh0c530f3_0  conda-forge     Cached
+# + pkg-config           0.29.2  ha3d46e9_1008 conda-forge     Cached
+    meson setup builddir --python.install-env auto
+    meson compile -C builddir
+    meson install -C builddir
+}
+if [[ $1 == isolate_build ]]
+then
+    isolate_build
+else
+    run_build
+fi
